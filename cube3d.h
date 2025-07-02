7fa937499c3c67767992ecd0766a7a3abe9ea2c3
Marouane Laamiri <malaamir@e3r8p13.1337.ma>
2025-07-02 14:36:48 +0100

    update

--- cube3d.h
+++ cube3d.h
@@ -6,12 +6,13 @@
 /*   By: malaamir <malaamir@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/01 11:24:36 by malaamir          #+#    #+#             */
-/*   Updated: 2025/07/01 11:25:56 by malaamir         ###   ########.fr       */
+/*   Updated: 2025/07/01 18:05:54 by malaamir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 // # include "get_next_line.h"
 // # include "../MLX42/include/MLX42/MLX42.h"
+
 # include <fcntl.h>
 # include <math.h>
 # include <unistd.h>
