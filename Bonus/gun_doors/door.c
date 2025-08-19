6c8cb2339878abb5de37a2496c4ce7bd7b5581d5
Marouane Laamiri <malaamir@e3r3p6.1337.ma>
2025-08-19 19:05:23 +0100

    1337

--- Bonus/gun_doors/door.c
+++ Bonus/gun_doors/door.c
@@ -6,13 +6,13 @@
 /*   By: malaamir <malaamir@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/13 20:49:46 by malaamir          #+#    #+#             */
-/*   Updated: 2025/08/18 23:50:14 by malaamir         ###   ########.fr       */
+/*   Updated: 2025/08/19 18:40:23 by malaamir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d_bonus.h"
 
-static int	sgnf(float v)
+static int	tile_direction(float v)
 {
 	if (v > 0.5f)
 		return (1);
@@ -30,8 +30,8 @@ void	toggle_door(t_data *data)
 
 	py = (int)(data->player_y / TILE_SIZE);
 	px = (int)(data->player_x / TILE_SIZE);
-	tx = px + sgnf(cosf(data->ray_angle));
-	ty = py + sgnf(sinf(data->ray_angle));
+	tx = px + tile_direction(cosf(data->ray_angle));
+	ty = py + tile_direction(sinf(data->ray_angle));
 	if (data->map[ty][tx] == DOOR_CLOSED)
 		data->map[ty][tx] = DOOR_OPEN;
 	else if (data->map[ty][tx] == DOOR_OPEN)
